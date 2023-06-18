6f1047273e8ac65e52e0f12708a70d973b1b02aa
Muhammadazim <odiljonov150@gmail.com>
2023-06-18 01:33:30 -0700

    Muhammadazim first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tesla/screens/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,7 +16,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Tesla App',),
+      home: const HomeScreen(),
     );
   }
 }
