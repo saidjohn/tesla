e659d36876404b8e7e08c9cba2864fd70657f70c
saidjohn <saidjon.ibn.usmon@gmail.com>
2023-06-18 11:38:37 +0500

    first commit

---
+++ lib/services/themes/colors.dart
@@ -0,0 +1,38 @@
+import 'dart:ui';
+
+class AppColors {
+  AppColors._();
+
+  /// dark version
+  static const backgroundDark = Color(0xFF24272C);
+  static const darkShadowDark = Color(0xFF000000);
+  static const darkLabelDark = Color(0xFF7F8493);
+
+
+  /// light version
+
+  static const backgroundlight = Color(0xFFE9EAF0);
+  static const lightShadowlight = Color(0xFFFFFFFF);
+  static const lightLabellight = Color(0xFF757E95);
+
+  /// gradient
+  static const gradient = [
+    Color(0xFF2FB8FF),
+    Color(0xFF9EECD9),
+  ];
+
+  /// background unlockPageGradient color
+  static const lockPageGradient = [
+    Color(0xFF292C31),
+    Color(0xFF000000),
+    Color(0xFF000000),
+    Color(0xFF292929),
+  ];
+
+  /// background unlockPageGradient color
+  static const unlockPageGradient = [
+    Color(0xFF2A2D32),
+    Color(0xFF161719),
+  ];
+
+}
