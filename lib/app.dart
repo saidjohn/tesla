9d25d8599f90f0f0cd98ce28c0cd72b23ba2ba44
abduraimovdev <kenzo778118@gmail.com>
2023-06-18 07:40:00 -0700

    Updated Screens And images Updated And Intro Screen Successfully

--- lib/app.dart
+++ lib/app.dart
@@ -1,6 +1,7 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:tesla/screens/climate_screen.dart';
+import 'package:tesla/screens/intro_page.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
@@ -14,7 +15,7 @@ class App extends StatelessWidget {
       theme: ThemeData(
         useMaterial3: true,
       ),
-      home: const ClimateScreen(),
+      home: const IntroScreen(),
     );
   }
 }
